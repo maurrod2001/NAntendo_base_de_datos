70d3d69b9e120b7217159eece8fe8be5c25a9096
cabiver <carlosvergel01@gmail.com>
2021-10-10 15:29:18 -0500

    fixed

--- index.py
+++ index.py
@@ -16,16 +16,17 @@ mycursor.execute("CREATE TABLE tabla_categoria (ID_categoria INT AUTO_INCREMENT 
   +"nombre_categoria VARCHAR(255) not null)")
 
 mycursor.execute("CREATE TABLE tabla_sede (ID_sede INT AUTO_INCREMENT PRIMARY KEY,"
- +"nombre VARCHAR(255) not null),"
- +"pais VARCHAR(255) not null),"
+ +"nombre VARCHAR(255) not null,"
+ +"pais VARCHAR(255) not null,"
  +"ciudad VARCHAR(255) not null)")
 
 mycursor.execute("CREATE TABLE tabla_personal (ID_personal int AUTO_INCREMENT PRIMARY KEY,"
- +"nombre VARCHAR(255) not null, apellido VARCHAR(255),"
+ +"nombre VARCHAR(255) not null,"
+ +"apellido VARCHAR(255),"
  +"cedula INT not null,"
- +"nombre DATE not null)")
+ +"nacimiento DATE not null)")
 
-mycursor.execute("CREATE TABLE tabla_presupuesto (ID_presupuesto int PRIMARY KEY,"
+mycursor.execute("CREATE TABLE tabla_presupuesto (ID_presupuesto INT PRIMARY KEY,"
  +"encargado int,"
  +"desarrolladores int,"
  +"herramientas int,"
@@ -40,6 +41,13 @@ mycursor.execute("CREATE TABLE tabla_patrocinadores (ID_patrocinador int AUTO_IN
  +"pais VARCHAR(255),"
  +"ciudad VARCHAR(255))")
 
+mycursor.execute("CREATE TABLE tabla_grupo (ID_grupo INT not null PRIMARY KEY,"
+ +"ID_personal INT not null," 
+ +"ID_cargo INT not null,"
+ +"FOREIGN KEY (ID_personal) REFERENCES tabla_personal(ID_personal),"
+ +"FOREIGN KEY (ID_cargo) REFERENCES tabla_cargo(ID_cargo))")
+
+
 mycursor.execute("CREATE TABLE tabla_proyectos (ID_proyecto int AUTO_INCREMENT PRIMARY KEY,"
  +"nombre_proyecto VARCHAR(255) not null,"
  +"ID_encargado INT,"
@@ -47,15 +55,9 @@ mycursor.execute("CREATE TABLE tabla_proyectos (ID_proyecto int AUTO_INCREMENT P
  +"ID_sede INT,"
  +"ID_patrocinador INT,"
  +"ID_presupuesto INT,"
- +"FOREIGN KEY (ID_encargado) REFERENCES tala_personal(ID_personal),"
+ +"FOREIGN KEY (ID_encargado) REFERENCES tabla_personal(ID_personal),"
  +"FOREIGN KEY (ID_grupo) REFERENCES tabla_grupo(ID_grupo),"
- +"FOREIGN KEY (ID_sede) REFERENCES tabla_sede(ID_sede),"
  +"FOREIGN KEY (ID_patrocinador) REFERENCES tabla_patrocinadores(ID_patrocinador),"
- +"FOREIGN KEY (ID_presupuesto) REFERENCES tabla_sede(ID_presupuesto),"
+ +"FOREIGN KEY (ID_presupuesto) REFERENCES tabla_presupuesto(ID_presupuesto),"
  +"FOREIGN KEY (ID_sede) REFERENCES tabla_sede(ID_sede))")
 
-mycursor.execute("CREATE TABLE tabla_grupo (ID_grupo INT not null,"
- +"ID_personal INT not null," 
- +"ID_cargo INT not null,"
- +"FOREIGN KEY (ID_personal) REFERENCES tabla_personal(ID_personal),"
- +"FOREIGN KEY (ID_cargo) REFERENCES tabla_cargo(ID_cargo))")
